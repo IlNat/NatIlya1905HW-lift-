b97bcc35c071efaa0b09cb10439d37bfe67f5d17
IlNat <ilanatalcenko18@gmail.com>
2022-05-29 23:42:10 +0500

    Fix mistakes.

--- lift_class.cpp
+++ lift_class.cpp
@@ -1,1 +1,52 @@
 #include "lift_class.h"
+using namespace std;
+
+
+void liftClass::setFloor(int floor)
+{
+	floorForNow = floor;
+}
+
+void liftClass::setMaxFloor()
+{
+	cout << "Введите номер верхнего этажа: ";
+	cin >> maxFloor;
+}
+
+void liftClass::printFloorForNow()
+{
+	cout << floorForNow;
+}
+
+void liftClass::controlLift()
+{
+	int floor;
+	do {
+		cout << "Вызвать лифт на...\n";
+		cin >> floor;
+		if (floor < 1 || floor > maxFloor || floor == floorForNow)
+		{
+			cout << "Неправильно введён номер этажа. Повторите ввод.\n";
+		}
+		else
+		{
+			setFloor(floor);
+			cout << "В настоящий момент времени лифт на ";
+			printFloorForNow();
+			cout << " этаже." << endl;
+		}
+	} while (floor < 0 && floor > maxFloor && floor == floorForNow);
+}
+
+void liftClass::setFirstFloor()
+{
+	if (floorForNow == 1)
+	{
+		cout << "Лифт и так находится на первом этаже." << endl;
+	}
+	else
+	{
+		floorForNow = 1;
+		cout << "Лифт находится на 1 этаже." << endl;
+	}
+}
--- lift_class.h
+++ lift_class.h
@@ -12,58 +12,10 @@ private:
 public: 
 	bool working;
 
-	bool checkSettingFloor(int floor) 
-	{
-
-	}
-
-	void setFloor(int floor) 
-	{ 
-		floorForNow = floor; 
-	}
-
-	void setMaxFloor() 
-	{
-		cout << "Введите номер верхнего этажа: ";
-		cin >> maxFloor;
-	}
-
-	void printFloorForNow() 
-	{ 
-		cout << floorForNow; 
-	}
-
-	void controlLift()
-	{
-		int floor;
-		do {
-			cout << "Вызвать лифт на...\n";
-			cin >> floor;
-			if (floor < 1 || floor > maxFloor || floor == floorForNow)
-			{
-				cout << "Неправильно введён номер этажа. Повторите ввод.\n";
-			}
-			else
-			{
-				setFloor(floor);
-				cout << "В настоящий момент времени лифт на ";
-				printFloorForNow();
-				cout << " этаже." << endl;
-			}
-		} while (floor < 0 && floor > maxFloor && floor == floorForNow);
-	}
-
-	void setFirstFloor()
-	{
-		if (floorForNow == 1)
-		{
-			cout << "Лифт и так находится на первом этаже." << endl;
-		}
-		else 
-		{
-			floorForNow = 1;
-			cout << "Лифт находится на 1 этаже." << endl;
-		}
-	}
+	void setFloor(int floor);
+	void setMaxFloor();
+	void printFloorForNow();
+	void controlLift();
+	void setFirstFloor();
 };
 
